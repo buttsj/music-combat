81fdf38fdc78fa1402c2c4a46a1bd434cec011fb
Jack Butts <buttsjofficial@gmail.com>
2018-03-01 18:09:53 -0500

    Initial commit

---
+++ Assets/MusicSphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSphere : MonoBehaviour {
+
+    public void MusicSphereEventHandler(BeatDetection.EventInfo eventInfo)
+    {
+        if (eventInfo.messageInfo == BeatDetection.EventType.Kick)
+        {
+            GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<BeatDetection>().CallBackFunction = MusicSphereEventHandler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
